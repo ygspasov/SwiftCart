9fc259a4db192ad1f9131d488dc37ca241f53c38
Yavor Spassov <ygspasov@gmail.com>
2024-01-19 11:30:55 +0200

    Adding fake products data and displaying it on the FE - part 2

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <div><h1>Products page</h1></div>
+    <div>
+      <h1>Products page</h1>
+      <div v-for="product in products" :key="product.id">
+        {{ product }}
+        <!-- <img :src="product.imageUrl" alt="" /> -->
+      </div>
+    </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { products } from '../assets/fake-data';
+</script>
 <style scoped></style>
