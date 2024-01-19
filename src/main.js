2a1266b55fbdda6561b28292fa89b41d7d5119e0
Yavor Spassov <ygspasov@gmail.com>
2024-01-19 17:05:19 +0200

    Adding Vuetify 3

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
 
-const app = createApp(App)
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-app.use(createPinia())
-app.use(router)
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.mount('#app')
+import router from './router';
+
+const app = createApp(App).use(vuetify);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
