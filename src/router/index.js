7fc4710227bb29211db37630cf26f6702e718156
Yavor Spassov <ygspasov@gmail.com>
2024-01-24 03:17:12 +0200

    Adding the NotFoundPage component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import CartPage from '@/views/CartPage.vue';
 import ProductDetailPage from '@/views/ProductDetailPage.vue';
 import ProductsPage from '@/views/ProductsPage.vue';
+import NotFoundPage from '@/views/NotFoundPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,14 +26,7 @@ const router = createRouter({
       path: '/',
       redirect: '/products',
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundPage },
   ],
 });
 
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-col>
+      <v-row class="">
+        <p class="text-center text-h2 mx-auto">404: Page Not Found</p>
+      </v-row>
+    </v-col>
+  </v-container>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
