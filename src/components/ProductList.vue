3279fb99f21b097e3b4b509def8e7e5972d25152
Yavor Spassov <ygspasov@gmail.com>
2024-01-25 19:53:10 +0200

    Adding the ProductList and ProductListItem components

---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-list
+    v-for="item in cartItems"
+    :key="item.id"
+    class="d-flex flex-wrap flex-column flex-md-row justify-space-between mx-5 my-2"
+  >
+    <ProductListItem :item="item" />
+    <v-divider class="mt-5"></v-divider>
+  </v-list>
+</template>
+<script setup>
+import ProductListItem from '@/components/ProductListItem.vue';
+const props = defineProps(['cartItems']);
+const cartItems = props.cartItems;
+</script>
+<style scoped></style>
---
+++ src/components/ProductListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <v-img :width="200" cover :src="item.imageUrl" class="mx-auto"></v-img>
+  </div>
+
+  <div class="d-flex flex-column text-h6 mx-2 my-2 my-md-0 flex-grow-1 text-center text-md-left">
+    <span class="flex-grow-1 text-h5">{{ item.name }}</span>
+    <span class="flex-grow-1 text-h5">${{ item.price }}</span>
+  </div>
+  <div class="d-flex align-start mx-auto mx-md-0">
+    <v-btn class="" color="black">Remove From Cart</v-btn>
+  </div>
+</template>
+<script setup>
+const props = defineProps(['item']);
+const item = props.item;
+</script>
+<style scoped></style>
