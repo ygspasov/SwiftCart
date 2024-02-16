b85a367699b4634455dc7b822d2af9303f35fff2
Yavor Spassov <ygspasov@gmail.com>
2024-02-16 14:39:04 +0200

    Styling changes to the ProductDetailPage

--- src/views/shop/ProductDetailPage.vue
+++ src/views/shop/ProductDetailPage.vue
@@ -11,22 +11,22 @@
         ></v-img>
       </v-col>
     </v-row>
-    <v-row class="d-flex justify-center">
+    <v-row class="d-flex justify-center my-3" no-gutters>
       <v-col class="" md="3"
         ><div class="text-left text-h6">{{ product.name }}</div></v-col
       ><v-col class="" md="3"
         ><div class="text-right font-weight-bold">${{ product.price }}</div></v-col
       >
     </v-row>
-    <v-row class="d-flex justify-center">
+    <v-row class="d-flex justify-center my-3" no-gutters>
       <v-col md="6"
         ><p class="text-left">Average rating: {{ product.averageRating }}</p></v-col
       >
     </v-row>
-    <v-row class="d-flex justify-center"
-      ><v-col class="" md="6"><v-btn class="w-100">Add to Cart</v-btn></v-col></v-row
+    <v-row class="d-flex justify-center my-3" no-gutters
+      ><v-col class="" md="6"><v-btn class="w-100" color="black">Add to Cart</v-btn></v-col></v-row
     >
-    <v-row class="d-flex justify-center">
+    <v-row class="d-flex justify-center my-3" no-gutters>
       <v-col md="6"
         ><p class="text-left font-weight-bold mb-2 text-subtitle-1">Description</p>
         <p>
