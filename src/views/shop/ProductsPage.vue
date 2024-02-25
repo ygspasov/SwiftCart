5346eeab102326f9aeea4b1991f0a42852993c68
Yavor Spassov <ygspasov@gmail.com>
2024-02-25 06:10:23 +0200

    Fixing a problem with products not updating after creating a new product and routing to the list of products

--- src/views/shop/ProductsPage.vue
+++ src/views/shop/ProductsPage.vue
@@ -8,21 +8,24 @@
   </div>
 </template>
 <script setup>
-import { onMounted, ref } from "vue";
+import { ref, onMounted, onBeforeMount } from "vue";
 import axios from "axios";
 import ProductsGrid from "@/components/ProductsGrid.vue";
 let products = ref([]);
 
 const getProducts = async () => {
   try {
-    const result = await axios.get("http://localhost:8000/api/products");
+    const result = await axios.get("/api/products");
     products.value = result.data;
     console.log(result.data);
   } catch (err) {
     console.log("err", err);
   }
 };
-
+const beforeRouteEnter = async () => {
+  await getProducts();
+};
+onBeforeMount(beforeRouteEnter);
 onMounted(() => {
   getProducts();
 });
