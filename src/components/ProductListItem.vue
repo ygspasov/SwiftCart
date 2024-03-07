75b1d6877315e064c445515983f40d6f12c7f882
Yavor Spassov <ygspasov@gmail.com>
2024-03-07 11:19:12 +0200

    Fixing an issue with the cart not updating after deleting an item

--- src/components/ProductListItem.vue
+++ src/components/ProductListItem.vue
@@ -3,7 +3,9 @@
     <v-img :width="200" cover :src="item.productData.imageUrl" class="mx-auto"></v-img>
   </div>
 
-  <div class="d-flex flex-column text-h6 mx-2 my-2 my-md-0 flex-grow-1 text-center text-md-left">
+  <div
+    class="d-flex flex-column text-h6 mx-2 my-2 my-md-0 flex-grow-1 text-center text-md-left"
+  >
     <span class="flex-grow-1 text-h5">{{ item.productData.name }}</span>
     <span class="flex-grow-1 text-h5"> Quantity: {{ item.qty }}</span>
     <span class="flex-grow-1 text-h5">Price: ${{ item.productData.price }}</span>
@@ -13,23 +15,23 @@
   </div>
 </template>
 <script setup>
-import axios from 'axios';
-import { useCartStore } from '@/stores/cart';
+import axios from "axios";
+import { useCartStore } from "@/stores/cart";
 const store = useCartStore();
 const { loadCart } = store;
-const props = defineProps(['item']);
+const props = defineProps(["item"]);
 const item = props.item;
 
-const RemoveCartItem = () => {
+const RemoveCartItem = async () => {
   const productId = item.productData.id;
-  console.log('productId', productId);
+  console.log("productId", productId);
   try {
-    axios.delete(`/api/cart/${productId}`, {
+    await axios.delete(`/api/cart/${productId}`, {
       productId,
     });
     loadCart();
   } catch (err) {
-    console.log('Error removing item:', err);
+    console.log("Error removing item:", err);
   }
 };
 </script>
