a04ca98fa546ac2472829b74f2d8ab30c82a6ee9
Yavor Spassov <ygspasov@gmail.com>
2024-03-30 00:00:51 +0200

    Adding the ProductsPage template and displaying orders from the db

--- src/views/shop/OrdersPage.vue
+++ src/views/shop/OrdersPage.vue
@@ -1,3 +1,39 @@
-<template><h1 class="mx-auto">Orders</h1></template>
-<script setup></script>
+<template>
+  <h1>Orders</h1>
+
+  <div v-for="order in orders" :key="order._id" :title="'Order id ' + order._id">
+    <v-list lines="one">
+      <v-list-item
+        v-for="product in order.items.cartItems"
+        :key="product._id"
+        :title="'Product: ' + product.name"
+        :subtitle="'Description: ' + product.description"
+      >
+        <p>Price: {{ product.price }}</p>
+      </v-list-item>
+      <v-list-item
+        ><p>Оrder price: {{ order.items.totalPrice }}</p></v-list-item
+      >
+    </v-list>
+  </div>
+</template>
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const orders = ref([]);
+
+const getOrders = async () => {
+  try {
+    let result = await axios.get('/api/orders');
+    orders.value = result.data;
+    console.log('orders.value', orders.value);
+  } catch (err) {
+    console.log('Error removing item:', err);
+  }
+};
+onMounted(() => {
+  getOrders();
+});
+</script>
 <style scoped></style>
