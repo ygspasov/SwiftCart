15db41558a48eb1bf11f39261261102c3db88d63
Yavor Spassov <ygspasov@gmail.com>
2024-03-31 12:24:12 +0300

    Adding an order cart to the orders page

--- src/views/shop/OrdersPage.vue
+++ src/views/shop/OrdersPage.vue
@@ -1,15 +1,16 @@
 <template>
   <h1>Orders</h1>
 
-  <div v-for="order in orders" :key="order._id" :title="'Order id ' + order._id">
+  <div v-for="order in orders" :key="order._id">
+    <p>Order # {{ order._id }}</p>
     <v-list lines="one">
-      <v-list-item
-        v-for="product in order.items.cartItems"
-        :key="product._id"
-        :title="'Product: ' + product.name"
-        :subtitle="'Description: ' + product.description"
-      >
-        <p>Price: {{ product.price }}</p>
+      <v-list-item v-for="product in order.items.cartItems" :key="product._id">
+        <v-card
+          :subtitle="'Description: ' + product.description"
+          :text="product.price"
+          :title="'Product: ' + product.name"
+          class="w-100"
+        ></v-card>
       </v-list-item>
       <v-list-item
         ><p>Оrder price: {{ order.items.totalPrice }}</p></v-list-item
@@ -18,18 +19,18 @@
   </div>
 </template>
 <script setup>
-import { onMounted, ref } from 'vue';
-import axios from 'axios';
+import { onMounted, ref } from "vue";
+import axios from "axios";
 
 const orders = ref([]);
 
 const getOrders = async () => {
   try {
-    let result = await axios.get('/api/orders');
+    let result = await axios.get("/api/orders");
     orders.value = result.data;
-    console.log('orders.value', orders.value);
+    console.log("orders.value", orders.value);
   } catch (err) {
-    console.log('Error removing item:', err);
+    console.log("Error removing item:", err);
   }
 };
 onMounted(() => {
