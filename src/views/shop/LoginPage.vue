3c791be453e599724274de1e7db22054474e9b2b
Yavor Spassov <ygspasov@gmail.com>
2024-05-10 07:56:25 +0300

    Adding validation for the email and password fields of the login form

--- src/views/shop/LoginPage.vue
+++ src/views/shop/LoginPage.vue
@@ -1,8 +1,28 @@
 <template>
   <v-sheet class="mx-auto mt-5" width="300">
     <v-form fast-fail @submit.prevent>
-      <v-text-field v-model="state.userEmail" label="Email"></v-text-field>
-      <v-text-field v-model="state.userPassword" label="Password" type="password"></v-text-field>
+      <v-text-field
+        v-model="state.userEmail"
+        label="Email"
+        @input="v$.userEmail.$touch()"
+      ></v-text-field>
+      <div :class="{ error: v$.userEmail.$errors.length }">
+        <div class="input-errors" v-for="error of v$.userEmail.$errors" :key="error.$uid">
+          <div class="text-red mb-2">{{ error.$message }}</div>
+        </div>
+      </div>
+
+      <v-text-field
+        v-model="state.userPassword"
+        label="Password"
+        type="password"
+        @input="v$.userPassword.$touch()"
+      ></v-text-field>
+      <div :class="{ error: v$.userPassword.$errors.length }">
+        <div class="input-errors" v-for="error of v$.userPassword.$errors" :key="error.$uid">
+          <div class="text-red mb-2">{{ error.$message }}</div>
+        </div>
+      </div>
       <v-btn class="mt-2" type="submit" block @click="login">Login</v-btn>
     </v-form>
   </v-sheet>
@@ -30,7 +50,7 @@ const rules = {
   userEmail: { required, email },
   userPassword: { required },
 };
-const v$ = useVuelidate(rules);
+const v$ = useVuelidate(rules, state);
 console.log('v$', v$);
 
 const login = async () => {
