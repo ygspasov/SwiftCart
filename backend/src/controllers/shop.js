ef80bb4584f3b572ae0f414baf0528c200cbdc0e
Yavor Spassov <ygspasov@gmail.com>
2024-06-04 08:02:38 +0300

    Formatting the pdf invoice to vertically align columns

--- backend/src/controllers/shop.js
+++ backend/src/controllers/shop.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { Product } from '../models/product.js';
 import { Order } from '../models/order.js';
 
-import PDFdocument from 'pdfkit';
+import PDFDocument from 'pdfkit';
 
 const getProductsController = async (req, res) => {
   try {
@@ -70,34 +70,58 @@ const getInvoiceController = (req, res, next) => {
         if (!order) {
           return next(new Error('No order found.'));
         }
-        //checking if the order belongs to the currently logged in user in order to allow file download
+        // Checking if the order belongs to the currently logged-in user to allow file download
         if (order.user.userId.toString() !== req.user._id.toString()) {
           return next(new Error('Unauthorized'));
         }
+
         const invoiceName = 'invoice-' + orderId + '.pdf';
         const invoicePath = path.join('backend', 'src', 'assets', 'invoices', invoiceName);
-        const pdfDoc = new PDFdocument();
+
+        const pdfDoc = new PDFDocument();
+
+        // Setting headers for the response
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', `attachment; filename="${invoiceName}"`);
+
         pdfDoc.pipe(fs.createWriteStream(invoicePath));
         pdfDoc.pipe(res);
-        pdfDoc.fontSize(24).text('Invoice');
-        pdfDoc.fontSize(16).text('--------------------------------------------');
+
+        // Table headers
+        const nameX = 50;
+        const quantityX = 300;
+        const priceX = 400;
+        let y = 100;
+
+        pdfDoc.fontSize(24).text('Invoice', nameX, y, { align: 'left' });
+        y = 120;
+        pdfDoc.fontSize(14).text('---------------------------------', nameX, y, { align: 'left' });
+
+        pdfDoc.fontSize(12).text('Name', nameX, 150, { underline: true });
+        pdfDoc.text('Quantity', quantityX, 150, { underline: true });
+        pdfDoc.text('Price', priceX, 150, { underline: true });
+        y = 170;
+        // Table rows
         order.products.forEach((prod) => {
-          pdfDoc
-            .fontSize(14)
-            .text(prod.product.name + ' - ' + prod.quantity + ' x ' + '$' + prod.product.price);
+          pdfDoc.fontSize(10).text(prod.product.name, nameX, y);
+          pdfDoc.text(prod.quantity, quantityX, y);
+          pdfDoc.text(`$${prod.product.price}`, priceX, y);
+          y += 10; // Moving to next line
         });
-        pdfDoc.fontSize(16).text('--------------------------------------------');
-        pdfDoc.fontSize(18).text('Total Price ' + '$' + order.totalPrice);
+
+        y += 10;
+
+        pdfDoc.fontSize(14).text('---------------------------------', nameX, y, { align: 'left' });
+        y += 20;
+        pdfDoc.fontSize(16).text('Total Price: $' + order.totalPrice, nameX, y, { align: 'left' });
+
         pdfDoc.end();
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"');
       })
       .catch((err) => next(err));
   } catch (error) {
     return next(error);
   }
 };
-
 export {
   getProductsController,
   getProductIdController,
