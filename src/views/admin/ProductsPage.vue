537c966685a2219cbc70b589a6bdd6d53cc80eaf
Yavor Spassov <ygspasov@gmail.com>
2024-06-14 17:07:41 +0300

    Making the left and right pagination buttons work.

--- src/views/admin/ProductsPage.vue
+++ src/views/admin/ProductsPage.vue
@@ -14,7 +14,7 @@
             <v-pagination
               v-model="page"
               :length="totalPages"
-              @click="handlePageChange"
+              @update:modelValue="handlePageChange"
               class="my-4"
             ></v-pagination>
           </v-container>
@@ -23,28 +23,28 @@
     </v-container>
   </div>
 </template>
+
 <script setup>
-import { onMounted, ref, provide, watch } from 'vue';
+import { ref, onMounted, watch, provide } from 'vue';
 import { useRouter } from 'vue-router';
 import axios from 'axios';
 import { useAlertsStore } from '@/stores/alerts';
 import ShopAlerts from '@/components/ShopAlerts.vue';
 import ProductsGrid from '@/components/ProductsGrid.vue';
+
 const alertsStore = useAlertsStore();
 const router = useRouter();
-let products = ref([]);
 provide(/* key */ 'admin', /* value */ true);
-let page = router.currentRoute.value.query.page
-  ? parseInt(router.currentRoute.value.query.page)
-  : 1; // Initialize with page 1 or from query parameter
+const products = ref([]);
+let page = ref(
+  router.currentRoute.value.query.page ? parseInt(router.currentRoute.value.query.page) : 1
+); // Initialize with page 1 or from query parameter
 const totalPages = ref(1);
+
 const getProducts = async (page) => {
   try {
-    //Set how many products should load on a single page
     const limitPerPage = 3;
     const result = await axios.get(`/api/admin/admin-products?page=${page}&limit=${limitPerPage}`);
-    console.log('page', page);
-    console.log('result', result);
     products.value = result.data.products;
     totalPages.value = result.data.totalPages;
   } catch (err) {
@@ -56,24 +56,19 @@ const getProducts = async (page) => {
 watch(
   () => router.currentRoute.value.query.page,
   (newPage) => {
-    console.log('newPage', newPage);
     if (!isNaN(parseInt(newPage))) {
-      page = parseInt(newPage);
-      getProducts(page);
+      page.value = parseInt(newPage);
+      getProducts(page.value);
     }
   }
 );
 
 // Handle pagination change
-const handlePageChange = (event) => {
-  // Extracting the page value from the event
-  const newPage = event.target.innerText;
-  // Updating the router's query parameter with the new page value
+const handlePageChange = (newPage) => {
   router.push({ query: { ...router.currentRoute.value.query, page: newPage } });
 };
 
 onMounted(() => {
-  getProducts(page);
+  getProducts(page.value);
 });
 </script>
-<style scoped></style>
