2909ae488415f2381bc9504b4308ae11f178df8a
Yavor Spassov <ygspasov@gmail.com>
2024-07-02 20:39:18 +0300

    Preparing for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,34 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
-      '/images': 'http://localhost:8000',
-      '/auth': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  const backendUrl = isProduction
+    ? 'https://swiftcart-7801bc3a5de9.herokuapp.com'
+    : 'http://localhost:8000';
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: backendUrl,
+          changeOrigin: true,
+        },
+        '/images': {
+          target: backendUrl,
+          changeOrigin: true,
+        },
+        '/auth': {
+          target: backendUrl,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
