e42ef2d4711212a562ccf26d3692479b216ac48f
Yavor Spassov <ygspasov@gmail.com>
2024-07-12 16:32:39 +0300

    moving the cypress env vars to .env

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:5173', // Make sure this matches your dev server URL
+    env: {
+      userEmail: process.env.CYPRESS_USER_EMAIL,
+      userPassword: process.env.CYPRESS_USER_PASSWORD,
+    },
+  },
+  component: {
+    specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite',
+    },
+  },
+});
