2d0b6e5933871799132c4e9e7361d3af6214f3b8
Yavor Spassov <ygspasov@gmail.com>
2024-07-30 10:54:07 +0300

    Adding the Add Category modal

--- src/views/admin/ProductsPage.vue
+++ src/views/admin/ProductsPage.vue
@@ -1,7 +1,52 @@
 <template>
   <div>
     <v-container>
-      <h3 class="text-center mb-2 open-sans-regular">Admin products</h3>
+      <div class="pa-4 text-center">
+        <v-dialog v-model="dialog" max-width="600">
+          <template v-slot:activator="{ props: activatorProps }">
+            <v-btn
+              class="text-none font-weight-regular"
+              prepend-icon="mdi-plus"
+              text="Add category"
+              variant="tonal"
+              v-bind="activatorProps"
+            ></v-btn>
+          </template>
+
+          <v-card title="Add new category">
+            <v-card-text>
+              <v-row dense>
+                <v-col cols="12">
+                  <v-text-field label="Category name*" required></v-text-field>
+                </v-col>
+
+                <v-col cols="12">
+                  <v-text-field label="Category description"></v-text-field>
+                </v-col>
+              </v-row>
+
+              <small class="text-caption text-medium-emphasis"
+                >*indicates required field</small
+              >
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
+
+              <v-btn
+                color="primary"
+                text="Save"
+                variant="tonal"
+                @click="dialog = false"
+              ></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
       <ShopAlerts :alert="alertsStore.alert" />
       <v-row align="baseline" justify="center">
         <ProductsGrid :products="products" />
@@ -25,30 +70,34 @@
 </template>
 
 <script setup>
-import { ref, onMounted, watch, provide } from 'vue';
-import { useRouter } from 'vue-router';
-import axios from 'axios';
-import { useAlertsStore } from '@/stores/alerts';
-import ShopAlerts from '@/components/ShopAlerts.vue';
-import ProductsGrid from '@/components/ProductsGrid.vue';
-
+import { ref, onMounted, watch, provide } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+import { useAlertsStore } from "@/stores/alerts";
+import ShopAlerts from "@/components/ShopAlerts.vue";
+import ProductsGrid from "@/components/ProductsGrid.vue";
+const dialog = ref(false);
 const alertsStore = useAlertsStore();
 const router = useRouter();
-provide(/* key */ 'admin', /* value */ true);
+provide(/* key */ "admin", /* value */ true);
 const products = ref([]);
 let page = ref(
-  router.currentRoute.value.query.page ? parseInt(router.currentRoute.value.query.page) : 1
+  router.currentRoute.value.query.page
+    ? parseInt(router.currentRoute.value.query.page)
+    : 1
 ); // Initialize with page 1 or from query parameter
 const totalPages = ref(1);
 
 const getProducts = async (page) => {
   try {
     const limitPerPage = 6;
-    const result = await axios.get(`/api/admin/admin-products?page=${page}&limit=${limitPerPage}`);
+    const result = await axios.get(
+      `/api/admin/admin-products?page=${page}&limit=${limitPerPage}`
+    );
     products.value = result.data.products;
     totalPages.value = result.data.totalPages;
   } catch (err) {
-    console.log('Error fetching products:', err);
+    console.log("Error fetching products:", err);
   }
 };
 
