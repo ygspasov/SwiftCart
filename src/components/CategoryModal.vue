3be09fde11d4ad0fa2466332df42fbb75d7599a6
Yavor Spassov <ygspasov@gmail.com>
2024-07-30 11:38:09 +0300

    Moving the category modal to a separate component

---
+++ src/components/CategoryModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="pa-4 text-center">
+    <v-dialog v-model="dialog" max-width="600">
+      <template v-slot:activator="{ props: activatorProps }">
+        <v-btn
+          class="text-none font-weight-regular"
+          prepend-icon="mdi-plus"
+          text="Add category"
+          variant="tonal"
+          v-bind="activatorProps"
+        ></v-btn>
+      </template>
+
+      <v-card title="Add new category">
+        <v-card-text>
+          <v-row dense>
+            <v-col cols="12">
+              <v-text-field label="Category name*" required></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field label="Category description"></v-text-field>
+            </v-col>
+          </v-row>
+
+          <small class="text-caption text-medium-emphasis"
+            >*indicates required field</small
+          >
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
+
+          <v-btn
+            color="primary"
+            text="Save"
+            variant="tonal"
+            @click="dialog = false"
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const dialog = ref(false);
+</script>
+<style scoped></style>
--- src/views/admin/ProductsPage.vue
+++ src/views/admin/ProductsPage.vue
@@ -1,52 +1,7 @@
 <template>
   <div>
+    <CategoryModal />
     <v-container>
-      <div class="pa-4 text-center">
-        <v-dialog v-model="dialog" max-width="600">
-          <template v-slot:activator="{ props: activatorProps }">
-            <v-btn
-              class="text-none font-weight-regular"
-              prepend-icon="mdi-plus"
-              text="Add category"
-              variant="tonal"
-              v-bind="activatorProps"
-            ></v-btn>
-          </template>
-
-          <v-card title="Add new category">
-            <v-card-text>
-              <v-row dense>
-                <v-col cols="12">
-                  <v-text-field label="Category name*" required></v-text-field>
-                </v-col>
-
-                <v-col cols="12">
-                  <v-text-field label="Category description"></v-text-field>
-                </v-col>
-              </v-row>
-
-              <small class="text-caption text-medium-emphasis"
-                >*indicates required field</small
-              >
-            </v-card-text>
-
-            <v-divider></v-divider>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
-
-              <v-btn
-                color="primary"
-                text="Save"
-                variant="tonal"
-                @click="dialog = false"
-              ></v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </div>
       <ShopAlerts :alert="alertsStore.alert" />
       <v-row align="baseline" justify="center">
         <ProductsGrid :products="products" />
@@ -76,7 +31,8 @@ import axios from "axios";
 import { useAlertsStore } from "@/stores/alerts";
 import ShopAlerts from "@/components/ShopAlerts.vue";
 import ProductsGrid from "@/components/ProductsGrid.vue";
-const dialog = ref(false);
+import CategoryModal from "@/components/CategoryModal.vue";
+
 const alertsStore = useAlertsStore();
 const router = useRouter();
 provide(/* key */ "admin", /* value */ true);
